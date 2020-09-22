ba735980b5638ef5dd25ba3932f67e1375799048
Kyle Coffey <mariorules24@gmail.com>
2020-09-21 19:58:19 -0500

    Rearrange ArgMeta struct for optimal padding

--- src/arglex.hh
+++ src/arglex.hh
@@ -45,70 +45,52 @@ struct Arg
 
 struct ArgMeta
 {
-  std::string argv0;
-
-  std::optional<std::string> record;
-  std::optional<std::string> recordFrom;
-  std::optional<RecordFromTo> recordFromTo;
-
-  std::optional<std::string> playdemo;
-  std::optional<std::string> timedemo;
-  std::optional<std::string> fastdemo;
-
   std::vector<std::string> wads;
   std::vector<std::string> dehs;
   std::vector<std::string> lmps;
-
-  std::optional<int32_t> complevel;
-
-  std::optional<uint32_t> width;
-  std::optional<uint32_t> height;
-  std::optional<uint8_t> viewAngle;
-  std::optional<VideoMode> vidMode;
-
-  bool fullscreen;
-  /// fullscreen for this session only
-  bool tempFullscreen;
-
-  std::optional<std::string> iwad;
   std::vector<std::string> files;
-
+  std::string argv0;
+  std::optional<std::string> record;
+  std::optional<std::string> recordFrom;
+  std::optional<std::string> playdemo;
+  std::optional<std::string> timedemo;
+  std::optional<std::string> fastdemo;
+  std::optional<std::string> iwad;
   std::optional<std::string> loadGame;
-  std::optional<Warp> warp;
-  std::optional<uint8_t> skill;
-
-  std::optional<uint32_t> turbo;
-
-  bool respawn;
-  bool fast;
-  bool noMonsters;
-
   std::optional<std::string> net;
-  std::optional<uint32_t> port;
-
-  bool soloNet;
-
-  std::optional<uint8_t> ffmap;
-
-  bool noSound;
-  bool noSfx;
-  bool noMusic;
-  bool noJoy;
-  bool noMouse;
-  bool noAccel;
-
-  std::optional<uint8_t> scaleFactor;
-
   std::optional<std::string> config;
   std::optional<std::string> save;
-
-  bool devparm;
   std::optional<std::string> debugFile;
-  bool noDraw;
-  bool noBlit;
   std::optional<std::string> bexOut;
-
+  std::optional<RecordFromTo> recordFromTo;
+  std::optional<int32_t> complevel;
+  std::optional<uint32_t> width;
+  std::optional<uint32_t> height;
+  std::optional<VideoMode> vidMode;
+  std::optional<uint32_t> turbo;
+  std::optional<uint32_t> port;
+  bool respawn = false;
+  bool fast = false;
+  bool noMonsters = false;
+  bool soloNet = false;
+  bool noSound = false;
+  bool noSfx = false;
+  bool noMusic = false;
+  bool noJoy = false;
+  bool noMouse = false;
+  bool noAccel = false;
+  bool devparm = false;
+  bool noDraw = false;
+  bool noBlit = false;
+  std::optional<uint8_t> viewAngle;
+  std::optional<bool> fullscreen;
+  /// fullscreen for this session only
+  std::optional<bool> tempFullscreen;
+  std::optional<uint8_t> skill;
+  std::optional<uint8_t> ffmap;
+  std::optional<uint8_t> scaleFactor;
   std::optional<uint8_t> dogs;
+  std::optional<Warp> warp;
 
   void handleLooseArg(const Arg &arg);
 };
